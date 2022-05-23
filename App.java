cac99579536727c8c3121cd3339ebb70fc4edcd0
scottbooma <scottbooma@gmail.com>
2022-05-23 17:13:54 -0400

    Add variables and print in readable string

--- App.java
+++ App.java
@@ -1,6 +1,24 @@
 class App {
   public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+    String vin = "1234567";
+    short year = 2015;
+    String makeAndModel = "Ford F-150";
+    String color = "Bronze Fire Metallic";
+    boolean hasTowingPackage = true;
+    int odometerReading = 74000;
+    String price = "$27,999";
+    char qualityRating = 'A';
+
+    StringBuilder vehicle = new StringBuilder();
+    vehicle.append(vin);
+    vehicle.append(String.format(" %d", year));
+    vehicle.append(String.format(" %s", makeAndModel));
+    vehicle.append(String.format(" %s", color));
+    vehicle.append(String.format(" %s", hasTowingPackage));
+    vehicle.append(String.format(" %d", odometerReading));
+    vehicle.append(String.format(" %s", price));
+    vehicle.append(String.format(" %c", qualityRating));
+
+    System.out.println(vehicle.toString());
   }
 }
